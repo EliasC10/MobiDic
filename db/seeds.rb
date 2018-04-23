2e853aa0ae264c8959d4179d7b8bfe94b2fa9d81
Elias Cia <ecia.mmt-b2016@fh-salzburg.ac.at>
2018-04-23 13:51:08 +0200

    fix: seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ Institution.create(name: "Puch Urstein")
 Institution.create(name: "Hallein")
 
 Supervisor.create(name: "Dr. Cle", password: "test", institution_id: 1)
-Supervisor.create(name: "Dr. Mens" password: "test" institution_id: 1)
+Supervisor.create(name: "Dr. Mens", password: "test", institution_id: 1)
 
 Client.create(name: "Eli", date_of_birth: "01.01.2018", institution_id: 1)
 Client.create(name: "As", date_of_birth: "01.01.2017", institution_id: 1)
@@ -20,6 +20,6 @@ Category.create(name: "Gesundheit", flag: "#8AC65A")
 Category.create(name: "Bildung", flag: "#F5D22D")
 
 
-EntryCategory.create(entry_id: 1 category_id: 1)
-EntryCategory.create(entry_id: 2 category_id: 2)
-EntryCategory.create(entry_id: 3 category_id: 3)
+EntryCategory.create(entry_id: 1, category_id: 1)
+EntryCategory.create(entry_id: 2, category_id: 2)
+EntryCategory.create(entry_id: 3, category_id: 3)
